0571141bb601cd2ac7394865920b7e3880aa3acb
anton.maignan <anton.maignan@emeria.eu>
2024-08-03 12:18:32 +0200

    new contact popup

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="relative flex justify-center py-8 max-w-screen-xl mx-auto">
+    <footer className="relative flex justify-center items-center py-8 max-w-screen-xl mx-auto">
       <div className="flex items-center gap-4">
-        <a href="https://www.linkedin.com/in/anton-maignan-165ab5297/" target="_blank" rel="noopener noreferrer" className="text-gray-400 border border-gray-400 p-2 text-xl rounded-full hover:text-white hover:bg-orange-500 hover:border-orange-500 transition-all duration-300">
+        <a href="https://www.linkedin.com/in/anton-maignan-165ab5297/" target="_blank" rel="noopener noreferrer" className="w-10 h-10 text-gray-400 border border-gray-400 p-2 text-xl rounded-full hover:text-white hover:bg-orange-500 hover:border-orange-500 transition-all duration-300 flex items-center justify-center">
           <i className="ri-linkedin-line"></i>
         </a>
-        <a href="https://github.com/AntonMAIGNAN" target="_blank" rel="noopener noreferrer" className="text-gray-400 border border-gray-400 p-2 text-xl rounded-full hover:text-white hover:bg-orange-500 hover:border-orange-500 transition-all duration-300">
+        <a href="https://github.com/AntonMAIGNAN" target="_blank" rel="noopener noreferrer" className="w-10 h-10 text-gray-400 border border-gray-400 p-2 text-xl rounded-full hover:text-white hover:bg-orange-500 hover:border-orange-500 transition-all duration-300 flex items-center justify-center">
           <i className="ri-github-line"></i>
         </a>
       </div>
